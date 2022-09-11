9cfc6872594ce8b16d13c92970865cf97e874eaa
AX <ananya.dhananjaya1998@gmail.com>
2022-09-11 21:47:35 +0530

    feat: Tagline animation

---
+++ pages/Mail.tsx
@@ -0,0 +1,13 @@
+const Mail = () => {
+  return (
+    <div>
+      <p>
+        I&apos;m currently available to get involved in new projects, so get in
+        touch if you&apos;d like to work together.
+      </p>
+      <p>Email me at ananya.dhananjaya1998@gmail.com</p>
+    </div>
+  )
+}
+
+export default Mail
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,19 +11,17 @@ function App() {
     <div className="mx-auto h-screen text-3xl text-slate-500 px-20">
       <div className="md:pt-32 pt-10">
         <p className="text-4xl font-mono">Ananya Dhananjaya</p>
-        <p className="text-lg font-mono">
-          Frontend Engineer growing a career in tech.
-        </p>
+        <p className={styles.header}></p>
       </div>
       <div className="bg-slate-600 w-1/3 mt-2 rounded border-2"></div>
       <div className="md:pt-10 pt-4">
         <p className="text-base font-mono">
-          Web designer and developer from Bangalore, India. I create custom
-          websites to help business to do better online. I enjoy buiding
-          everything from small business sites to rich interactive web apps. If
-          you are a business seeking a web presence, an employer looking to hire
-          or a fellow developer looking to collaborate, you can get in touch
-          with me!
+          Web designer and developer from Bangalore, growing a career in tech. I
+          create custom websites to help business to do better online. I enjoy
+          buiding everything from small business sites to rich interactive web
+          apps. If you are a business seeking a web presence, an employer
+          looking to hire or a fellow developer looking to collaborate, you can
+          get in touch with me!
         </p>
       </div>
 
