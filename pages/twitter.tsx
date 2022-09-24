0221bba618d3fe90cbbbee3133c3c080d6cfe40f
AX <ananya.dhananjaya1998@gmail.com>
2022-09-25 00:02:16 +0530

    feat: Twitter info

--- pages/twitter.tsx
+++ pages/twitter.tsx
@@ -1,7 +1,22 @@
 export default function Twitter() {
   return (
-    <div>
-      <p>Head to my twitter!</p>
+    <div className="h-96 w-screen flex justify-center items-center ">
+      <div>
+        <p className="font-poppins tracking-wide">
+          I occasionally tweet about life and tech.
+        </p>
+        <p className="font-poppins tracking-wide">
+          {`Let's connect on `}
+          <span>
+            <a
+              href="https://twitter.com/ui_ananya"
+              className="underline underline-offset-4 text-blue-500 hover:text-blue-600 visited:text-indigo-700"
+            >
+              twitter!
+            </a>
+          </span>
+        </p>
+      </div>
     </div>
   )
 }
