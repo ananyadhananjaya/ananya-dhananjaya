47e05fbaa1deaa83a8c9d628501e28ecb1dd884c
AX <ananya.dhananjaya1998@gmail.com>
2022-09-25 00:25:21 +0530

    feat: Email content

--- pages/Mail.tsx
+++ pages/Mail.tsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-
 const Mail = () => {
-  const [showCopy, setShowCopy] = useState<boolean>(false)
-  return <div></div>
+  return (
+    <div className="h-96 flex justify-center items-center flex-col font-poppins tracking-wide">
+      <p>
+        Feel free to drop an Email. However, please expect a delay in my
+        response.
+      </p>
+      <p>
+        Click to copy{' '}
+        <span
+          className=" text-slate-800 hover:text-slate-900 hover:underline hover:cursor-pointer"
+          onClick={() => {
+            navigator.clipboard.writeText('ananya.dhananjaya@gmail.com')
+          }}
+        >
+          ananya.dhananjaya1998@gmail.com
+        </span>
+      </p>
+    </div>
+  )
 }
 
 export default Mail
